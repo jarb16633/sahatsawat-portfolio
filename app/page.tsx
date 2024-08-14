885fac149520768f28ca4527d2873a09219e815f
jarb16633 <sahassavas.rim@gmail.com>
2024-08-15 00:13:31 +0700

    Clients section with infinite moving cards animation

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
