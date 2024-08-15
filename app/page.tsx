7f76d01309d42802937ac687380d95aab069441d
jarb16633 <sahassavas.rim@gmail.com>
2024-08-15 10:44:34 +0700

    Section update

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -17,6 +17,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
--- components/Clients.tsx
+++ components/Clients.tsx
@@ -4,7 +4,7 @@ import { companies, testimonials } from "@/data";
 
 const Clients = () => {
   return (
-    <div className="py-20" id="projects">
+    <div className="py-20" id="testimonials">
       <h1 className="heading">
         Kind words from
         <span className="text-purple"> satisfied clients</span>
@@ -16,7 +16,7 @@ const Clients = () => {
           speed="slow"
         />
 
-        <div>
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg: mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
             <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
               <img src={img} alt={name} className="md:w-10 w-5" />
